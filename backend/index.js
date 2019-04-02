f1c55cb2926c5fc95f682fa6370e08c912a03b51
Monish Narwani <narwanimonish@gmaill.com>
2019-04-02 22:33:48 +0530

    Added cors

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
 const port = 3000
 
 const employeesRoutes = require('./routes/employees.js')
 
 app.use(express.json())
 
-app.use('/api/employees', employeesRoutes)
+app.use('/api/employees', cors(), employeesRoutes)
 
 app.get('/', (req, res) => {
     res.send("Its working")
