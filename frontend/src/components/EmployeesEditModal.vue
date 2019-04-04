2d23553fc9158c177682b8bad965827bb171df75
Monish Narwani <narwanimonish@gmaill.com>
2019-04-04 08:23:24 +0530

    modal title updated

--- frontend/src/components/EmployeesEditModal.vue
+++ frontend/src/components/EmployeesEditModal.vue
@@ -4,7 +4,7 @@
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title">Modal title</h5>
+                        <h5 class="modal-title">Edit employee - {{emp.name}}</h5>
                         <button type="button" class="close" @click="closeEdit">
                             <span aria-hidden="true">&times;</span>
                         </button>
